790a2a5dc79f991af63c6f2496c3acf4eaec50d1
Sajith <sajimedia@gmail.com>
2018-09-14 12:38:05 +0530

    base64-to-image dependancy is added

---
+++ index.js
@@ -0,0 +1,16 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+ dl  = require('delivery');
+
+app.get('/', function(req, res) {
+   res.sendfile('index.html');
+});
+
+io.on('connection', function(socket) {
+   console.log('A user connected');
+});
+
+http.listen(3000, function() {
+   console.log('listening on localhost:3000');
+});
