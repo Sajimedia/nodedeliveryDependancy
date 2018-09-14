496dd9c78eaa6b5c88fa8e20f4f50f6d278bb6b5
Sajith <sajimedia@gmail.com>
2018-09-14 12:59:05 +0530

    delivery package is added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
- dl  = require('delivery');
+var dl  = require('delivery');
+var base64ToImage = require('base64-to-image');
+
 
 app.get('/', function(req, res) {
    res.sendfile('index.html');
@@ -9,7 +11,16 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket) {
    console.log('A user connected');
-});
+   socket.on('image',(data)=>{
+   	var base64Str = data;
+   	//console.log(data);
+    var path ='uploadIMG/'; 
+    var optionalObj = {'fileName': 'ssss', 'type':'jpg'};
+    base64ToImage(base64Str,path,optionalObj);
+	});
+ });  
+ 
+
 
 http.listen(3000, function() {
    console.log('listening on localhost:3000');
